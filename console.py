d009fdb9f3f4a05ff7f152d7a39d61b1ea7b33f5
Alex Eftimie <alex@eftimie.ro>
2011-06-16 23:01:50 +0300

    search fixed, install and remove stubs; async doesn't work because of gi

--- console.py
+++ console.py
@@ -3,8 +3,12 @@
 import sys
 from gi.repository import PackageKitGlib as packagekit
 
-def progress_cb(status, type, data=None):
-    pass
+def progress_cb(status, typ, data=None):
+    if status.get_property('package'):
+        print "Pachet ", status.get_property('package'), status.get_property('package-id')
+        if status.get_property('package'):
+            print status.get_property('package').get_name()
+    print typ, status.get_property('package')
 
 def main(args):
     if len(args) < 2:
@@ -16,9 +20,20 @@ def main(args):
         if len(args) < 3:
             print "Nothing to search"
             return -1
-        result = client.search_names(0, args[2], None, progress_cb, None)
-        pkgs = result.get_details_array()
-        print pkgs
+        result = client.search_names(0, [args[2],], None, progress_cb, None)
+        pkgs = result.get_package_array()
+        for p in pkgs:
+            print p.get_name()
+    elif args[1] == 'install':
+        client.install_packages(False, [args[2],], None, progress_cb, None)
+    elif args[1] == 'remove':
+        client.remove_packages([args[2],],
+                                False, # allow_deps
+                                False, # autoremove
+                                None,  # cancelable
+                                progress_cb,
+                                None
+        )
     else:
         print "Unknown command: ", args[1]
 if __name__ == "__main__":
